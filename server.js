b2ef6c32b066fd6dccc08aac0afe5cecf8c37186
Daniel Kim <daniel.seungchul.kim@gmail.com>
2020-02-04 15:27:03 -0500

    added morgan to see reqs, serve up node modules on our express server, and added coreui and tested basic functionality

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use("/", express.static(path.join(__dirname + "/public")));
+
+app.get("/node_modules/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname + req.path));
+});
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
