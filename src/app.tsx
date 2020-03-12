63d44bf4edbb8e7f8439f3af3edf6c841b10bd98
esther-baek <esther.baek@ibm.com>
2020-03-12 16:13:41 -0400

    add header component

--- src/app.tsx
+++ src/app.tsx
@@ -4,14 +4,37 @@ import BlogList from './components/BlogList';
 import ViewBlog from './components/ViewBlog'
 import { Switch, Link, Route } from 'react-router-dom';
 
+import './styles.scss';
+import {
+  Header,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenu,
+  HeaderMenuItem
+} from "carbon-components-react/lib/components/UIShell";
+
 export default function App() {
   return (
     <div className="container">
-      <div><Link to='/'>Home</Link></div>
+      <Header aria-label="IBM Platform Name">
+      <HeaderName href="#" prefix="IBM">
+        [Platform]
+      </HeaderName>
+      <HeaderNavigation aria-label="IBM [Platform]">
+        <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
+        <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
+        <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+        <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
+          <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
+          <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
+          <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
+        </HeaderMenu>
+      </HeaderNavigation>
+    </Header>
+      {/* <div><Link to='/'>Home</Link></div>
       <div><Link to='/writeblog'>WriteBlog</Link></div>
       <a href="/appid/logout">Logout</a>
-      <hr />
-
+      <hr /> */}
       <Switch>
         <Route path='/writeblog' render={() => <WriteBlog />} />
         <Route path='/viewBlog' render={({ location }) => <ViewBlog location={location} />} />
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,15 @@ const config = {
         use: ["style-loader", "css-loader"]
       },
       {
+        test: /\.scss$/,
+        use: [ 
+          { loader: "style-loader" },  // to inject the result into the DOM as a style block
+          { loader: "css-modules-typescript-loader"},  // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+          { loader: "css-loader", options: { modules: true } },  // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+          { loader: "sass-loader" },  // to convert SASS to CSS 
+        ]
+      },
+      {
         test: /\.png$/,
         use: [
           {
@@ -33,7 +42,7 @@ const config = {
     ]
   },
   resolve: {
-    extensions: [".js", ".tsx", ".ts"]
+    extensions: [".js", ".tsx", ".ts", ".css", ".scss"]
   }
 };
 
