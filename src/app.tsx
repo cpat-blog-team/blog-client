dff86b89e7d890e6196edc43c12c03bfc88bf48a
isaiah-santala <isaiah.santala@ibm.com>
2020-03-13 09:39:46 -0700

    adds basic nav bar without hamburger menu

--- src/app.tsx
+++ src/app.tsx
@@ -3,30 +3,17 @@ import { useState, useEffect } from "react";
 import userContext from "./userContext";
 import axios from "axios";
 
-import { Switch, Link, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import NavBar from './components/NavBar'
 import WriteBlog from "./components/WriteBlog";
 import BlogList from "./components/BlogList";
 import ViewBlog from "./components/ViewBlog";
-
-import './index.scss';
-
-import {
-  Header,
-  HeaderName,
-  HeaderNavigation,
-  HeaderMenu,
-  HeaderMenuItem
-} from "carbon-components-react/lib/components/UIShell";
-
-import { Button } from 'carbon-components-react';
-
 
 export default function App() {
   const [userData, setUserData] = useState({ name: "", email: "" });
   useEffect(() => {
     const getUserData: any = async () => {
       const { data } = await axios.get("/user");
-      console.log(data);
       setUserData(data);
     };
     getUserData();
@@ -34,32 +21,8 @@ export default function App() {
 
   return (
     <userContext.Provider value={userData}>
+      <NavBar />
       <div className="container">
-      <Button>HELLO</Button>
-        <Header aria-label="IBM Platform Name">
-          <HeaderName href="#" prefix="IBM">
-            [Platform]
-          </HeaderName>
-          <HeaderNavigation aria-label="IBM [Platform]">
-            <HeaderMenuItem href="#">POTATO</HeaderMenuItem>
-            <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
-            <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
-            <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
-              <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
-              <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
-              <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
-            </HeaderMenu>
-          </HeaderNavigation>
-        </Header>
-        <div>
-          <Link to="/">Home</Link>
-        </div>
-        <div>
-          <Link to="/writeblog">WriteBlog</Link>
-        </div>
-        <a href="/appid/logout">Logout</a>
-        <hr />
-
         <Switch>
           <Route path="/writeblog" component={WriteBlog} />
           <Route
