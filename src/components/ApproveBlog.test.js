a423e7826409f65c6b7bca50ce2ab80c0b4bf90c
ebaek <esther.baek@ibm.com>
2020-04-23 14:03:47 -0400

    Add approval blog route and modal

--- src/components/ApproveBlog.test.js
+++ src/components/ApproveBlog.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render, wait } from '@testing-library/react';
-import { exampleList, exampleBlogPost } from './exampleBlogPost';
-import userContext from "../userContext";
-import ApproveBlog from './ApproveBlog';
+// import React from 'react';
+// import { render, wait } from '@testing-library/react';
+// import { exampleList, exampleBlogPost } from './exampleBlogPost';
+// import userContext from "../userContext";
+// import ApproveBlog from './ApproveBlog';
 
 // describe('ApproveBlog component', () => {
 // })
