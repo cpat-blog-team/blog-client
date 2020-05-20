bf67b92cf189fef097cb1e6a8eacf9ab757212a3
isaiah-santala <isaiah.santala@ibm.com>
2020-05-20 16:17:12 -0700

    updates notification to close itself after 30 seconds

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastNotification } from 'carbon-components-react';
 
 interface Props {
@@ -12,22 +12,26 @@ interface Props {
 export default function BlogList({ handleClose, subtitle, title, kind }: Props) {
 	const [ notificationClassName, setNotificationClassName ] = useState('slide-in');
 
+	const close = () => {
+		setNotificationClassName('slide-out');
+		// wait for animation before running call back
+		setTimeout(handleClose, 1000);
+	};
+
+	// set notification to auto close after 30 seconds
+	useEffect(() => {
+		const timer = setTimeout(close, 30000);
+	}, []);
+
 	return (
 		<ToastNotification
 			kind={kind}
 			caption={
-				<a
-					href="#"
-					onClick={() => {
-						setNotificationClassName('slide-out');
-						handleClose();
-					}}
-				>
+				<a href="#" onClick={close}>
 					Dismiss
 				</a>
 			}
 			subtitle={subtitle}
-			timeout={10000}
 			title={title}
 			style={{
 				position: 'fixed',
